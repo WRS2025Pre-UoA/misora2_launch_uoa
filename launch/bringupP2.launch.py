4423688552347543754a6d161d854f730460242a
rui323 <ruiiwata837@gmail.com>
2025-04-10 10:56:45 +0900

    launch file in misora2

---
+++ launch/bringupP2.launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node, ComposableNodeContainer, LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    container = ComposableNodeContainer(
+        name="misora_uoa_part", 
+        package="rclcpp_components",
+        executable="component_container_mt",#マルチスレッドの場合component_container_mt,シングルはcomponent_container
+        namespace="",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="misora2_distribute_image",
+                plugin="component_distribute_image::DistributeImage",
+                name="distribute_image",
+                extra_arguments=[{"use_intra_process_comms": True}],
+                parameters=[{"mode": "P2"}]
+            ),
+            ComposableNode(
+                package="misora2_qr",
+                plugin="component_qr::DetectQR",
+                name="qr",
+                extra_arguments=[{"use_intra_process_comms": True}],
+            )
+        ],
+        output="screen",
+    )
+  
+    
+    # load_composable_nodes = LoadComposableNodes(
+    #     target_container="my_container",
+    #     composable_node_descriptions=[
+    #         ComposableNode(
+    #             package="listener",
+    #             plugin="Listener",
+    #             name="listener",
+    #             extra_arguments=[{"use_intra_process_comms": True}],
+    #         ),
+    #     ],
+    # )
+    
+    return LaunchDescription([
+        container, 
+        # load_composable_nodes
+    ])
