7ac702b7b4e4c4beaa9e2f8e45c4f13ec7b3a7f7
rui323 <ruiiwata837@gmail.com>
2025-09-19 10:07:55 +0900

    change send param

--- launch/bringupP3.launch.py
+++ launch/bringupP3.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
                 plugin="component_distribute_image::DistributeImage",
                 name="distribute_image",
                 extra_arguments=[{"use_intra_process_comms": True}],
-                parameters=[{"mode": "P3"}, {"check_duration_sec": 2.0}, {"timer_interval_ms": 200}], #画像を何millisec間隔で流すか、また何s間流すか],
+                parameters=[{"mode": "P3"}, {"check_duration_sec": 1.0}, {"timer_interval_ms": 100}], #画像を何millisec間隔で流すか、また何s間流すか],
                 remappings=[("raw_image" , "/arm_color_thermal_camera/selected_image_raw/compressed")]
             ),
             ComposableNode(
